0d508c082a42f2f57ebf1a296586bdd5d8fc1de8
purusottam32 <kingspuru@gmail.com>
2025-05-11 01:11:31 +0530

    carousel updated

--- src/components/Corousel.jsx
+++ src/components/Corousel.jsx
@@ -1,17 +1,19 @@
-// CarouselComponent.jsx
 import React from "react";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { useMediaQuery } from "react-responsive";
 
 function Corousel() {
+  const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
+
   const banners = [
-    { src: "/Banner1.webp", alt: "Banner 1" },
-    { src: "/banner2.webp", alt: "Banner 2" },
-    { src: "/banner3.webp", alt: "Banner 3" },
+    { src: "/Banner1.webp", mob: "/mobileBanner1.webp", alt: "Banner 1" },
+    { src: "/banner2.webp", mob: "/mobilebanner2.webp", alt: "Banner 2" },
+    { src: "/banner3.webp", mob: "/mobilebanner3.webp", alt: "Banner 3" },
   ];
 
   return (
-    <div className="container mx-auto shadow-sm mt-4">
+    <div className="container mx-auto shadow-sm mt-4 p-4">
       <Carousel
         showThumbs={false}
         showStatus={false}
@@ -23,7 +25,13 @@ function Corousel() {
       >
         {banners.map((item, index) => (
           <div key={index}>
-            <img src={item.src} alt={item.alt} className="w-full object-cover max-h[200px] md:max-h-[350px]" />
+            <img
+              src={isMobile ? item.mob : item.src}
+              alt={item.alt}
+              className={`w-full object-cover ${
+                isMobile ? "h-[180px]" : "max-h-[350px]"
+              } rounded-2xl`}
+            />
           </div>
         ))}
       </Carousel>
